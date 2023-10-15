dc1e96d4e88dc30bb3ef43011cdcf27ad68d6fd1
Faisal Karim <faisalchy64@gmail.com>
2023-10-16 00:54:23 +0600

    create - db.js database file and connect to database

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./db");
 
 const app = express();
 
 // use middlewares
 app.use(express.json());
 app.use(cors());
+
+// connect database
+connectDB();
 
 // routes
 app.get("/", (req, res) => {
