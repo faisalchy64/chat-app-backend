4f961815e5509020a5a32695b0a1afb8ad12a237
Faisal Karim <faisalchy64@gmail.com>
2023-10-19 14:09:30 +0600

    add - create and update conversation functionality on conversationController.js controller

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,3 +1,5 @@
+const Conversation = require("../models/conversationModel");
+
 const getConversations = async (req, res, next) => {
     res.send("getConversations");
 };
@@ -6,6 +8,37 @@ const getConversation = async (req, res, next) => {
     res.send("getConversation");
 };
 
-const postConversation = async (req, res, next) => {};
+const postConversation = async (req, res, next) => {
+    try {
+        const { message, sender, receiver } = req.body;
+        const conversation = await Conversation.findOne({
+            $or: [
+                { "sender._id": sender._id, "receiver._id": receiver._id },
+                { "sender._id": receiver._id, "receiver._id": sender._id },
+            ],
+        });
+
+        if (conversation) {
+            const updatedConversation = await Conversation.updateOne(
+                {
+                    _id: conversation._id,
+                },
+                { message, sender, receiver }
+            );
+
+            res.send(updatedConversation);
+        } else {
+            const createdConversation = await Conversation.create({
+                message,
+                sender,
+                receiver,
+            });
+
+            res.send(createdConversation);
+        }
+    } catch (err) {
+        next({ message: "Sent message failed!" });
+    }
+};
 
 module.exports = { getConversations, getConversation, postConversation };
